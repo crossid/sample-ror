8f249ae311267076bd54dcfa800e536592bbae91
Rani Sharim <rani@crossid.io>
2021-10-20 15:08:54 +0300

    Add / path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/", to: "login#index"
   get "/login", to: "login#index"
   get "/login/redirect", to: "login#login_redirect"
   get "/callback", to: "login#handle_callback"
